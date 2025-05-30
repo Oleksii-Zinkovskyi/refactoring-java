acedac6b27da048cdcfcc15c7c5c8548a9a7e823
Oleksii Zinkovskyi <Oleksii.Zinkovskyi@outlook.com>
2025-05-30 16:59:03 +0200

    feature-A001-code-refactoring Introduced testing dependencies, improved test coverage
    I added some positive and negative test cases to document the current behavior of the system and provide a baseline for sanity checks.
    Some edge cases appear to not be handled properly, but it will be addressed later.
    
    P.S. This is also the point where I had to patch and undo A LOT of my progress just so I can go back in commit history and amend the "Simple Maven transformation" commit. I didn't notice most of my files not getting picked up by the git add when I moved them to new packages, so I had to go back and fix it. Oooof >_<

--- src/test/java/com/etraveli/practice/RentalInfoTest.java
+++ src/test/java/com/etraveli/practice/RentalInfoTest.java
@@ -1,9 +1,13 @@
 package com.etraveli.practice;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RentalInfoTest {
 
@@ -11,7 +15,76 @@ public class RentalInfoTest {
     public void givenValidCustomerAndRentals_whenGenerateStatement_thenReturnInfo() {
         String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
         String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
-        Assertions.assertEquals(expected, result);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    //Technically, a customer can rent two copies of the same movie, as unlikely as it may be
+    @Test
+    public void givenValidCustomerWithDuplicateRentals_whenGenerateStatement_thenReturnInfo() {
+        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tYou've Got Mail\t9.5\n\tMatrix\t2.0\nAmount owed is 15.0\nYou earned 3 frequent points\n";
+        String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F001", 7), new MovieRental("F002", 1))));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    //TODO: Obviously throwing NPEs is not a good practice, I am merely documenting the current behavior
+    @Test
+    public void givenValidCustomerWithInvalidRental_whenGenerateStatement_thenThrowNPE() {
+        assertThrows(NullPointerException.class, () -> {
+            new RentalInfo().statement(new Customer("C. U. Stomer", List.of(new MovieRental("WRONG", 3))));
+        });
+    }
+
+    @Test
+    public void givenValidCustomerWithZeroDaysRental_whenGenerateStatement_thenReturnInfo() {
+        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t2.0\nAmount owed is 2.0\nYou earned 1 frequent points\n";
+        String result = new RentalInfo().statement(new Customer("C. U. Stomer", List.of(new MovieRental("F001", 0))));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    //TODO: This one is also odd, I am pretty sure we shouldn't allow negative days, but the original code does not handle it
+    @Test
+    public void givenValidCustomerWithMinDaysRental_whenGenerateStatement_thenReturnInfo() {
+        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t2.0\nAmount owed is 2.0\nYou earned 1 frequent points\n";
+        String result = new RentalInfo().statement(new Customer("C. U. Stomer", List.of(new MovieRental("F001", Integer.MIN_VALUE))));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    //TODO: So this isn't handled well either, we'll see if it can be somehow improved
+    @Test
+    public void givenValidCustomerWithMaxDaysRental_whenGenerateStatement_thenReturnInfo() {
+        String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.2212254695E9\nAmount owed is 3.2212254695E9\nYou earned 1 frequent points\n";
+        String result = new RentalInfo().statement(new Customer("C. U. Stomer", List.of(new MovieRental("F001", Integer.MAX_VALUE))));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void givenCustomerWithNoRentals_whenGenerateStatement_thenReturnInfo() {
+        String expected = "Rental Record for C. U. Stomer\nAmount owed is 0.0\nYou earned 0 frequent points\n";
+        String result = new RentalInfo().statement(new Customer("C. U. Stomer", Collections.emptyList()));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void givenCustomerWithNoNameOrRentals_whenGenerateStatement_thenReturnInfo() {
+        String expected = "Rental Record for \nAmount owed is 0.0\nYou earned 0 frequent points\n";
+        String result = new RentalInfo().statement(new Customer("", Collections.emptyList()));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    //TODO: Obviously throwing NPEs is not a good practice, I am merely documenting the current behavior
+    @Test
+    public void givenNullCustomerWithNullRentals_whenGenerateStatement_thenThrowNPE() {
+        assertThrows(NullPointerException.class, () -> {
+            new RentalInfo().statement(new Customer(null, null));
+        });
+    }
+
+    //TODO: Obviously throwing NPEs is not a good practice, I am merely documenting the current behavior
+    @Test
+    public void givenNoCustomer_whenGenerateStatement_thenThrowNPE() {
+        assertThrows(NullPointerException.class, () -> {
+            new RentalInfo().statement(null);
+        });
     }
 
 }
